c536ebc169f7a61208c77e9aed5d56b5f5b756c6
Dnguy104 <dnguy104@ucr.edu>
2017-06-08 16:55:06 -0700

    adding changes to heroku

--- index.js
+++ index.js
@@ -65,6 +65,24 @@ app.use('/', routes);
 app.use('/contact', contact);
 app.use('/project', project);
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.post('/', function(req, res) {
+    var first = req.body.First;
+    var last = req.body.Last;
+    var email = req.body.Email;
+    var message = req.body.Message;
+
+    res.send(first + last + email + message);
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
