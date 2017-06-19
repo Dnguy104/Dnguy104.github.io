b81546ce5b8911059ac46531ca546ac07c07c6df
Dnguy104 <dnguy104@ucr.edu>
2017-06-19 15:32:13 -0700

    removing node

--- routes/contact.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('contact', { title: 'Contact' });
-});
-
-module.exports = router;
